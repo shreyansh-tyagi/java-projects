f7e9e864b682869f1c027cf85b2a9df592e62697
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-05 22:24:58 +0530

    java basic program

---
+++ javabasics/src/javabasics/JavaBasics.java
@@ -0,0 +1,16 @@
+package javabasics;
+import java.util.Scanner;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of a and b: ");
+		
+		
+
+	}
+
+}
