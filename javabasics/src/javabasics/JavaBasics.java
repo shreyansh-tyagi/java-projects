39f7f950862a41f59ca22404df188930c8eeabcf
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-05 22:31:56 +0530

    java basic program

--- javabasics/src/javabasics/JavaBasics.java
+++ javabasics/src/javabasics/JavaBasics.java
@@ -2,15 +2,20 @@ package javabasics;
 import java.util.Scanner;
 
 public class JavaBasics {
+	Scanner sc=new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a,b;
-		Scanner sc=new Scanner(System.in);
+		
 		System.out.println("enter the value of a and b: ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=calculator(a,b);
 		
+	}
+	public static int calculator(int a,int b) {
+		String c=sc.nextLine();
 		
-
 	}
 
 }
