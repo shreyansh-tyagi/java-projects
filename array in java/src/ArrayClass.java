c4954f54e80fc7734df343eafbcffad06ce92cf0
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-06 15:47:40 +0530

    java basics projects

--- array in java/src/ArrayClass.java
+++ array in java/src/ArrayClass.java
@@ -13,14 +13,18 @@ public class ArrayClass {
 		{
 			arr[i]=sc.nextInt();
 		}
-		displaycontent();
+		displaycontent(arr);
 		
 
 	}
-	public static void displaycontent();
+	public static void displaycontent(int[] arr);
 	{
 		System.out.println("Enter 1 for reverse the array: \nEnter 2 for addition of array: \nEnter 3 sort the array: \nEnter 4 to find the length of array");
-		
+		int a=sc.nextInt();
+		switch(a) {
+		case 1:
+			
+		}
 	}
 
 }
