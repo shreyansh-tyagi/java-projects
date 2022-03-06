8e21de5e577356b16e92e601186a9cede63f8fff
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-06 16:11:06 +0530

    java basics projects

--- array in java/src/ArrayClass.java
+++ array in java/src/ArrayClass.java
@@ -17,14 +17,39 @@ public class ArrayClass {
 		
 
 	}
-	public static void displaycontent(int[] arr);
+	public static void displaycontent(int[] arr)
 	{
-		System.out.println("Enter 1 for reverse the array: \nEnter 2 for addition of array: \nEnter 3 sort the array: \nEnter 4 to find the length of array");
+		System.out.println("the array is displayed as: ");
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println("\nEnter 1 for reverse the array: \nEnter 2 for addition of array: \nEnter 3 sort the array: \nEnter 4 to find the length of array");
 		int a=sc.nextInt();
 		switch(a) {
 		case 1:
-			
+			int[] b=reverse(arr);
+			System.out.println("the reversed array is: ");
+			for(int i=0;i<b.length;i++) {
+				System.out.print(b[i]+" ");
+			}
+		case 2:
+			addition(int[] arr);
+		case 3: 
+			sort(int[] arr);
+		case 4:
+			System.out.println("the lenght of array is: "+arr.length);
 		}
+	}
+	
+	public static int[] reverse(int[] arr) {
+		int t,n=arr.length;
+		for(int i=0;i<arr.length;i++) {
+			t=arr[i];
+			arr[i]=arr[n];
+			arr[n]=t;
+			n--;
+		}
+		return arr;
 	}
 
 }
