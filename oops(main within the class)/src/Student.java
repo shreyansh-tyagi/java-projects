e5f0b94e393cf8f178f34a9063af891caf434fa6
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-07 23:44:08 +0530

    object creation in main class

--- oops(main within the class)/src/Student.java
+++ oops(main within the class)/src/Student.java
@@ -1,10 +1,13 @@
+import java.util.*;
 class Student {
-	System.out.println("enter the name: ");
-	Scanner.sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);
+	//System.out.println("enter the name: ");
 	String s=sc.nextLine();
-	System.out.println("enter the age: ");
+	//System.out.println("enter the age: ");
 	int a=sc.nextInt();
+	
 	public static void main(String args[]) {
+		
 		Student s1=new Student();
 		System.out.println("your name is: "+s1.s+"\nyour age is: "+s1.a);
 		
