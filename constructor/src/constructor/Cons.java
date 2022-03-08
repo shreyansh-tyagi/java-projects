602e919669698a1b172067392017e407e17b6f13
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-09 00:47:11 +0530

    constructor in java

--- constructor/src/constructor/Cons.java
+++ constructor/src/constructor/Cons.java
@@ -13,10 +13,13 @@ public class Cons {
 		age=a;
 	}
 	Cons(Cons c2){  //copy constructor
+		id=c2.id;
+		name=c2.name;
+		age=c2.age;
 		
 	}
 	void display() {
-		System.out.println("your name is: ")
+		System.out.println("your name is: "+name+"\n your id is: "+id+"\nyour age is:"+age);
 	}
 	
 
@@ -26,6 +29,7 @@ class Cons1{
 		Scanner sc=new Scanner(System.in);
 		Cons c=new Cons(sc.nextInt(),sc.nextLine());
 		Cons c1=new Cons(sc.nextInt(),sc.nextLine(),sc.nextInt());
+		Cons c2=new Cons(c1);
 		
 		
 	}
