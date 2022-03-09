146688640a6ac312b80937f99f32d8161c15b6eb
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-10 00:16:07 +0530

    static method

--- static method/src/StaticMethod.java
+++ static method/src/StaticMethod.java
@@ -1,12 +1,13 @@
 import java.util.*;
 public class StaticMethod {
-	int id;
+	static int id;
 	static String clgname="KIET";
 	StaticMethod(String s,int i){
 		id=i;
 		clgname=s;
+		
 	}
-	void display() {
+	static void display() {
 		System.out.println("your id is: "+id+"\nyour college name is: "+clgname);
 		
 	}
@@ -15,8 +16,8 @@ class Staticmethod1{
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
 		StaticMethod m=new StaticMethod(sc.nextLine(),sc.nextInt());
-		m.display();
-		m.id=12;
+		StaticMethod.display();
+		StaticMethod.id=12;
 		// m.clgname="HRIT"; this will not work as static variable and static method can only be accesseed by class name only
 		StaticMethod.clgname="HRIT";
 		System.out.println("new updated id is: "+m.id+"\nyour updated college is: "+StaticMethod.clgname);
