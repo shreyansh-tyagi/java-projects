bf9b2a36cb9dad05e8b8d93721b7c7e1b5ee156f
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-10 11:06:39 +0530

    single inheritence

--- single inheritence/src/StudentDetails.java
+++ single inheritence/src/StudentDetails.java
@@ -6,6 +6,9 @@ public class StudentDetails {
 		this.name=name;
 		this.age=age;
 	}
+	void display() {
+		System.out.println("your name: "+name+"\nyour age: "+age);
+	}
 }
 class StudentDetails1 extends StudentDetails{
 
@@ -15,15 +18,16 @@ class StudentDetails1 extends StudentDetails{
 		this.salary=salary;
 		System.out.println("your name: "+name+"\nyour age: "+age);
 	}
-	
-void display() {
-	System.out.println("your name: "+name+"\nyour age: "+age+"your salary is: "+salary);
-}
+	void display() {
+		System.out.println("your name: "+name+"\nyour age: "+age+"your salary is: "+salary);
+	}
 }
 class Mainmethod{
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
+		StudentDetails pobj=new StudentDetails(sc.nextLine(),sc.nextInt());
 		StudentDetails1 sobj=new StudentDetails1(sc.nextLine(),sc.nextInt(),sc.nextInt());
+		pobj.display();
 		sobj.display();
 		sc.close();
 	}
