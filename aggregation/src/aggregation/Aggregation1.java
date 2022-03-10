663f7dc8570c15ae5029d2d647a98be3b8875153
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-10 17:13:56 +0530

    interface in java

--- aggregation/src/aggregation/Aggregation1.java
+++ aggregation/src/aggregation/Aggregation1.java
@@ -2,29 +2,34 @@ package aggregation;
 import java.util.*;
 public class Aggregation1 {
 	int id,age,salary;
-	String name;
-	Aggregation1(int id,int age,int salary,String name){
+	Aggregation1(int id,int age,int salary){
 		this.id=id;
 		this.age=age;
 		this.salary=salary;
-		this.name=name;
+		
+	}
+	void display() {
+		System.out.println("id: "+id+"\nage: "+age+"\nsalary: "+salary);
 	}
 
 }
 class Aggregation2{
 	Aggregation1 catchup; //created an aggregation of class 1 and without inheriting accessing the element of class 1 through aggregation
-	Aggregation2(int id,int age,int salary,String name,Aggregation1 catchup){
+	Aggregation2(int id,int age,int salary,Aggregation1 catchup){
 		this.catchup=catchup;
 		
 	}
-	void display() {
-		System.out.println("name: "+catchup.name+"\nid: "+catchup.id+"\nage: "+catchup.age+"\nsalary: "+catchup.salary);
+	void display1() {
+		System.out.println("id is : "+catchup.id+"\nage is: "+catchup.age+"\nsalary is : "+catchup.salary);
 	}
 	public static void main(String args[]) {
 		Scanner sc=new Scanner(System.in);
-		Aggregation1 ag1=new Aggregation1(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextLine());
-		Aggregation2 ag2=new Aggregation2(sc.nextInt(),sc.nextInt(),sc.nextInt(),sc.nextLine(),ag1);
-		ag2.display();
+		System.out.println("enter the id age and salary: ");
+		Aggregation1 ag1=new Aggregation1(sc.nextInt(),sc.nextInt(),sc.nextInt());
+		System.out.println("enter the id age and salary for aggregation2: ");
+		Aggregation2 ag2=new Aggregation2(sc.nextInt(),sc.nextInt(),sc.nextInt(),ag1);
+		ag1.display();
+		ag2.display1();
 		sc.close();
 	}
 }
