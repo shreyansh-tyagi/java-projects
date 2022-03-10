6dafc981e74ab83d03bf54706952f1000d6be926
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-10 10:59:56 +0530

    single inheritence

--- single inheritence/src/StudentDetails.java
+++ single inheritence/src/StudentDetails.java
@@ -1,4 +1,24 @@
-
+import java.util.*;
 public class StudentDetails {
+	String name;
+	int age;
+	StudentDetails(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
+}
+class StudentDetails1 extends StudentDetails{
+
+	int salary;
+	StudentDetails1(String name, int age,int salary) {
+		super(name, age);
+		this.salary=salary;
+		System.out.println("your name: "+name+"\nyour age: "+age);
+	}
+	
+void display() {
+	System.out.println("your name: "+name+"\nyour age: "+age+"your salary is: "+salary);
+}
+
 
 }
