5e1243900ae6cb6006977c43b79a8c5d34d39dca
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-11 13:04:33 +0530

    covariant

--- covariant/src/covariant/Covariant.java
+++ covariant/src/covariant/Covariant.java
@@ -1,5 +1,28 @@
 package covariant;
-
+import java.util.*;
 public class Covariant {
 
+	Covariant method() {
+		return this;
+	}
+	void print() {
+		System.out.println("we are in parent class of covariant");
+	}
+}
+class Covariant1 extends Covariant{
+	Covariant method() {
+		return this;
+	}
+	void print() {
+		System.out.println("we are in subclass of covariant1");
+	}
+	
+}
+class Covariant2 extends Covariant1{
+	Covariant method() {
+		return this;
+	}
+	void print() {
+		System.out.println("we are in subclass of covariant2");
+	}
 }
