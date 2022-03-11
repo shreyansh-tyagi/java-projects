54b9b79125f4c1bef5b65bf56688768a30ab0f7e
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-11 16:15:20 +0530

    covariant

--- super program/src/Super1.java
+++ super program/src/Super1.java
@@ -1,18 +1,22 @@
 import java.util.*;
 public class Super1 {
 	
-	int n;
 	Scanner sc=new Scanner(System.in);
+	int n=sc.nextInt();
 	Super1(int n){
 		this.n=n;
 	}
+	
 	int[] add=new int[n];
-	int factorial1() {
+	
+	int factorial() {
 		System.out.println("enter the element into the array: ");
 		int fact=1;
+		System.out.println(n);
 		for(int i=0;i<n;i++) {
 			add[i]=sc.nextInt();
 		}
+		
 		for(int i=0;i<n;i++) {
 			fact*=add[i];
 		}
@@ -22,13 +26,18 @@ public class Super1 {
 }
 	
 class Super2 extends Super1{
-		Super2(int n){
-			super(n);	
-		}
 		
+		Super2(int n) {
+		super(n);
+		
+	}
+
+
 		void display() {
-		int a=super.factorial1();
+		int a=super.factorial();
 		System.out.println("the factorial is: "+a);}
+		
+		
 		int factorial() {
 			int sum=0;
 			for(int i=0;i<n;i++) {
@@ -46,7 +55,6 @@ class Mainmethod{
 		Super2 s=new Super2(sc.nextInt());
 		s.display();
 		int b=s.factorial();
-		
 		System.out.println("the addition is: "+b);
 		sc.close();
 		}
