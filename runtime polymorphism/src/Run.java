fe2e4f9f861cc5acd3427b260b946b19bcbf9b90
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-11 16:37:00 +0530

    run time polymorphism

--- runtime polymorphism/src/Run.java
+++ runtime polymorphism/src/Run.java
@@ -1,4 +1,23 @@
-
+import java.io.*;
 public class Run {
+	void running() {
+		System.out.println("parent class");
+	}
 
+}
+class Run1 extends Run{
+	void running() {
+		System.out.println("child class run1");
+	}
+}
+class Run2 extends Run1{
+	void running() {
+		System.out.println("we are run2 class");
+	}
+}
+class Runs extends Run{
+	void running() {
+		System.out.println("we are runs class");
+	}
+	
 }
