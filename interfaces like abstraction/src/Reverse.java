c8872f30fc28e48128d6c71816404dd099df88b8
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-15 11:19:43 +0530

    interface and abstraction in java

--- interfaces like abstraction/src/Reverse.java
+++ interfaces like abstraction/src/Reverse.java
@@ -12,9 +12,33 @@ class Logic implements Reverse,Evenodd{
 		System.out.println("enter the length of array: ");
 		int a=sc.nextInt();		
 		String s[]=new String[a];
-		
+		System.out.println("enter the string into the array: ");
+		for(int i=0;i<a;i++) {
+			s[i]=sc.nextLine();
+		}
+		System.out.println("entered string is: ");
+		for(String c:s) {
+			System.out.println(c);
+		}
+		String s1="";
+		for(int i=0;i<s.length/2;i++) {
+			s1=s[i];
+			s[i]=s[s.length-i-1];
+			s[s.length-i-1]=s1;
+		}
+		for(String c2:s) {
+			System.out.println(c2);
+		}
 	}
 	public void reverseword() {
 		
 	}
+	public static void main(String args[]) {
+		Logic l=new Logic();
+		l.reversestring();
+		l.reverseword();
+	}
 }
+
+
+
