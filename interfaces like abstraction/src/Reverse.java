521163028451e84599cfed265e2c28af396e0c75
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-15 11:36:34 +0530

    interface and abstraction in java

--- interfaces like abstraction/src/Reverse.java
+++ interfaces like abstraction/src/Reverse.java
@@ -1,24 +1,24 @@
 import java.util.*;
 public interface Reverse {
-	void reversestring();
+	void reversestring(); //by default abstract method
 
 }
 interface Evenodd {
 	void reverseword();
 }
-class Logic implements Reverse,Evenodd{
+class Logic implements Reverse,Evenodd{ //defining the abstract method by implementing interface
 	Scanner sc=new Scanner(System.in);
 	public void reversestring() {
 		System.out.println("enter the length of array: ");
 		int a=sc.nextInt();		
-		String s[]=new String[a];
+		String s[]=new String[a+1];
 		System.out.println("enter the string into the array: ");
-		for(int i=0;i<a;i++) {
+		for(int i=0;i<a+1;i++) {
 			s[i]=sc.nextLine();
 		}
-		System.out.println("entered string is: ");
+		System.out.println("\n\nentered string is: ");
 		for(String c:s) {
-			System.out.println(c);
+			System.out.print(c+" ");
 		}
 		String s1="";
 		for(int i=0;i<s.length/2;i++) {
@@ -26,11 +26,14 @@ class Logic implements Reverse,Evenodd{
 			s[i]=s[s.length-i-1];
 			s[s.length-i-1]=s1;
 		}
+		System.out.println("\n\nreversed array of  string is: ");
 		for(String c2:s) {
-			System.out.println(c2);
+			System.out.print(c2+" ");
 		}
 	}
 	public void reverseword() {
+		System.out.println("\n\nchoose the index from array which you want to reverse: ");
+		int b=sc.nextInt();
 		
 	}
 	public static void main(String args[]) {
