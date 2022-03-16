78e68265873a4c4d54f9597ee9144da8108d1c14
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-16 17:36:23 +0530

    threading in java

--- threads/src/threads/Threading.java
+++ threads/src/threads/Threading.java
@@ -4,9 +4,20 @@ public class Threading extends Thread{
 	public void run() {
 		System.out.println("thread is running");
 	}
+	
+
+}
+class Threading1 implements Runnable {
+	public void run() {
+		System.out.println("thread is running in interface");
+	}
+}
+class Mainthread{
 	public static void main(String args[]) {
 		Threading t=new Threading();
+		Threading1 t1=new Threading1();
+		Thread t2= new Thread(t1); //using the constructor thread 
+		t2.start();
 		t.start();
 	}
-
 }
