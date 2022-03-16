c85d66c40b4a777d0f3b8308307b8beaaa225a2b
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-16 16:02:40 +0530

    exception propagation

--- final, finally and finalize/src/Finalkeyword.java
+++ final, finally and finalize/src/Finalkeyword.java
@@ -4,12 +4,12 @@ public class Finalkeyword {
 void finalmethod(final int a) //the value of a cannot be change now as it is final value 
 {
 	Scanner sc =new Scanner(System.in);
-	System.out.println("enter the element to divide the final number: ");
+	System.out.println("\nenter the element to divide the final number: ");
 	int b=a/sc.nextInt();
 	try {
 		
 		
-		System.out.print("division with final integer is: "+b);
+		System.out.print("\ndivision with final integer is: "+b);
 	}
 	catch(Exception e) {
 		System.out.println(e);
@@ -17,17 +17,25 @@ void finalmethod(final int a) //the value of a cannot be change now as it is fin
 		
 	}
 	finally { //finally is the block which will execute no matter the exception occur or not 
-		System.out.println("finally the value of b: "+b);
-		sc.close();
+		System.out.println("\nfinally the value of b: "+b);
+		
 	}
+	sc.close();
 	
 }
+protected void  finalize() {
+	System.out.println("\nwe are in finalize method ");
+	
+}
+
 }
 class Mainfinal{
 	public static void main(String args[]) {
 		Scanner sc =new Scanner(System.in);
 		Finalkeyword f=new Finalkeyword();
+		System.out.println("enter the number: ");
 		f.finalmethod(sc.nextInt());
+		f.finalize();
 		sc.close();
 	}
 }
