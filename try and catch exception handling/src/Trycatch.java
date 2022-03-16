07f7d35a2882f0fbaf4779490358398366be6487
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-16 11:09:56 +0530

    exception handling

--- try and catch exception handling/src/Trycatch.java
+++ try and catch exception handling/src/Trycatch.java
@@ -1,4 +1,25 @@
 import java.util.*;
 public class Trycatch {
+	int i,j;
+	Trycatch(int i,int j){
+		this.i=i;
+		this.j=j;
+	}
 
+}
+class Trycatch1 extends Trycatch{
+	Trycatch1(int i,int j){
+		super(i,j);
+	}
+	int data;
+	void trycatchmethod() {
+	try {
+	 data = i/j;
+	}
+	catch(Exception e) {
+		System.out.println(e);
+		System.out.println("the division is: "+i/(j+2));
+		
+	}
+	}
 }
