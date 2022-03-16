835c2a3a1675229c59ce6ef4577584ef4758a6f5
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-16 15:12:29 +0530

    exception propagation

--- exception propagation/src/ExceptionPropagation.java
+++ exception propagation/src/ExceptionPropagation.java
@@ -1,4 +1,25 @@
 import java.util.*;
 public class ExceptionPropagation {
+	Scanner sc =new Scanner(System.in);
+	void a(int i,int j) {
+		int data=i/j;
+		System.out.println("the value of data: "+data);
+	}
+	void b() {
+		a(sc.nextInt(),sc.nextInt());
+	}
+	void c() {
+		try {
+			b();
+		}
+		catch(Exception e) {
+			System.out.println(e);
+			
+		}
+	}
+	public static void main(String args[]) {
+		ExceptionPropagation p=new ExceptionPropagation();
+		p.c();
+	}
 
 }
