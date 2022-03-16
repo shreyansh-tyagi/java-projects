0e2c45b74ba4766e87897f3b1ea9d73048affa84
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-16 15:46:45 +0530

    exception propagation

--- final, finally and finalize/src/Finalkeyword.java
+++ final, finally and finalize/src/Finalkeyword.java
@@ -18,12 +18,16 @@ void finalmethod(final int a) //the value of a cannot be change now as it is fin
 	}
 	finally { //finally is the block which will execute no matter the exception occur or not 
 		System.out.println("finally the value of b: "+b);
+		sc.close();
 	}
+	
 }
 }
 class Mainfinal{
 	public static void main(String args[]) {
 		Scanner sc =new Scanner(System.in);
-		Finalkeyword f=new Finalkeyword(sc.nextInt());
+		Finalkeyword f=new Finalkeyword();
+		f.finalmethod(sc.nextInt());
+		sc.close();
 	}
 }
