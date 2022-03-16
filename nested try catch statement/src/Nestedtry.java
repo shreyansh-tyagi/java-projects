302eb8303f9a6557f50fb3562cf643c9641c14da
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-16 12:14:02 +0530

    commited the exception

--- nested try catch statement/src/Nestedtry.java
+++ nested try catch statement/src/Nestedtry.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Nestedtry {
-	int i,j,n;
-	int arr[]=new int[n];
+	int i,j;
+	int arr[];
 	Nestedtry(int i,int j,int arr[]) {
 		this.i=i;
 		this.j=j;
@@ -22,20 +22,29 @@ class Nestedtry1 extends Nestedtry{
 		data=i/j;
 		System.out.println(data);
 		try {
+			try {
+				System.out.println("enter the size of array: ");
+				int n=sc.nextInt();				
+				arr[n]=i/j;
+				System.out.println("\n\nelement at "+n+"is: "+arr[n]);
+			}
+			catch(Exception e) {
+				
+			}
 			s=null;
 			System.out.println(s.length());
 			
 		}
-		catch(Exception e) {
-			System.out.println(e);
+		catch(Exception e1) {
+			System.out.println(e1);
 			System.out.println("\nenter the string: ");
 			s=sc.nextLine();
 			System.out.println("\nthe length of string: "+s.length());
 			
 		}
 	}
-	catch(Exception e) {
-		System.out.println(e);
+	catch(Exception e2) {
+		System.out.println(e2);
 		System.out.println("the division of two number: "+i/(j+2));
 		
 	}
