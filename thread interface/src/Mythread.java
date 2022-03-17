d6c1fafb494cafb56e4fbfd96bf65c213057f35e
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-17 13:53:00 +0530

    mythread

--- thread interface/src/Mythread.java
+++ thread interface/src/Mythread.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class Mythread extends Thread {
 	Scanner sc =new Scanner(System.in);
 	public void run() {
+		System.out.println("enter the number: ");
 		int a=sc.nextInt();
 		System.out.println("the value of b: "+a);
 	}
@@ -12,10 +13,14 @@ class Mythread1 implements Runnable{
 		System.out.println("we are in interface");
 	}
 }
-class Mainthread{
+class Mainthread1{
 	public static void main(String args[]) {
 		Mythread m=new Mythread();
+		//Thread t=new Thread(m); //optional //simply we use the class object to start the execution of thread
 		m.start();
+		Mythread1 m1=new Mythread1(); //while creating the interface we cannot access the method withou creating the object of thread class 
+		Thread t=new Thread(m1);
+		t.start();
 		
 	}
 }
