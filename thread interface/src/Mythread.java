40e5f5de1aa3e25436ec868f7b242cbf9cb81795
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-17 12:46:42 +0530

    mainthread where are you

--- thread interface/src/Mythread.java
+++ thread interface/src/Mythread.java
@@ -1,4 +1,21 @@
+import java.util.*;
+public class Mythread extends Thread {
+	Scanner sc =new Scanner(System.in);
+	public void run() {
+		int a=sc.nextInt();
+		System.out.println("the value of b: "+a);
+	}
 
-public class Mythread {
-
+}
+class Mythread1 implements Runnable{
+	public void run() {
+		System.out.println("we are in interface");
+	}
+}
+class Mainthread{
+	public static void main(String args[]) {
+		Mythread m=new Mythread();
+		m.start();
+		
+	}
 }
