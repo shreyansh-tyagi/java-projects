77df5057fbae8a07a62a022ca1bb0e2f4afc6e51
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-21 16:14:32 +0530

    daemon threading

--- daemon threading/src/Daemons.java
+++ daemon threading/src/Daemons.java
@@ -1,4 +1,22 @@
 
-public class Daemons {
+public class Daemons extends Thread{
+	public void run() {
+		if(Thread.currentThread().isDaemon()) {
+			System.out.println("yes it is daemon threading");
+		}
+		else {
+			System.out.println("no it not daemon");
+		}
+	}
+	public static void main(String args[]) {
+		Daemons d=new Daemons();
+		Daemons d1=new Daemons();
+		Daemons d2=new Daemons();
+		d.setDaemon(true);
+		d1.setDaemon(false);
+		d2.start();
+		d1.start();
+		d.start();
+	}
 
 }
