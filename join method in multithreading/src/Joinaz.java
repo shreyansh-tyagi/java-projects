683d15a2170930f36161a6d4cb289978e4b9558a
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-21 13:22:17 +0530

    join method in java

--- join method in multithreading/src/Joinaz.java
+++ join method in multithreading/src/Joinaz.java
@@ -1,4 +1,24 @@
 import java.util.*;
-public class Joinaz {
+public class Joinaz extends Thread{
+	Scanner sc=new Scanner(System.in);
+	public void run() {
+		System.out.println("enter the number: ");
+		int n=sc.nextInt();
+		int a = 1;
+		for(int i=0;i<=n;i++) {
+			try {
+				Thread.sleep(500);
+				a*=i;
+			}
+			catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			finally {
+				System.out.println("the multiplication is: "+a);
+			}
+			
+		}
+		System.out.println("the multiple of all number is: "+a);
+	}
 
 }
