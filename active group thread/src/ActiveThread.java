018ed9bae28d9de16a75472cfed00fbd86a5cf9a
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-21 17:19:16 +0530

    threados

--- active group thread/src/ActiveThread.java
+++ active group thread/src/ActiveThread.java
@@ -1,4 +1,28 @@
-
-public class ActiveThread {
-
+import java.util.*;
+public class ActiveThread extends Thread{
+	ActiveThread(String name,ThreadGroup abc){
+		super(abc,name);// put ThreadGroup type variable first and then put other variable inside the constructor
+		start();
+	}
+	public void run() {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for (int i=0;i<=n;i++) {
+			try {
+				Thread.sleep(500);
+			}
+			catch(InterruptedException e) {
+				System.out.println(e);
+			}
+		}
+sc.close();
+System.out.println(Thread.currentThread().getName()+"is the first execution");
+	}
+}
+class MainThreados{
+	public static void main(String args[]) {
+		ThreadGroup g=new ThreadGroup("the parent of threadGroup");
+		ActiveThread a=new ActiveThread("first execute",g);
+		ActiveThread b=new ActiveThread("second execute",g);
+	}
 }
