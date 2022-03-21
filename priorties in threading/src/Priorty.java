8e922e98cafcdb7ecce48c348cda60de7475fb31
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-21 15:53:27 +0530

    priorty in java

--- priorties in threading/src/Priorty.java
+++ priorties in threading/src/Priorty.java
@@ -1,8 +1,14 @@
-import java.util.*;
+//import java.util.*;
 public class Priorty extends Thread{
 	public void run() {
 		System.out.println("we are run method");
 	}
-	
-
+	public static void main(String args[]) {
+		Priorty p=new Priorty();
+		Priorty p1=new Priorty();
+		Priorty p2=new Priorty();
+		System.out.println("lets get the priorty of threads: "+p.getPriority());
+		System.out.println("lets get the priorty of threads: "+p1.getPriority());
+		System.out.println("lets get the priorty of threads: "+p2.getPriority());
+	}
 }
