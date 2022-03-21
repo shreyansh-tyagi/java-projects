f006370d050a95052930e435f755b63b3499a9e1
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-21 12:54:55 +0530

    run method ready to execute

--- run method in multithreading/src/Threados.java
+++ run method in multithreading/src/Threados.java
@@ -1,4 +1,34 @@
+import java.util.*;
+public class Threados extends Thread{
+	Scanner sc =new Scanner(System.in);
+	public void run() {
+		int a=0;
+		
+		System.out.println("enter the number: ");
+		int n=sc.nextInt();
+		for(int i=0;i<=n;i++) {
+			try {
+				Thread.sleep(500);
+				a+=i;
+			}
+			catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			finally {
+				System.out.println("the addition is: "+a);
+			}
+			
+		}
+		System.out.println("the sum of all numner is: "+a);
+	}
 
-public class Threados {
-
+}
+class Threados1 extends Threados {
+	public static void main(String args[]) {
+		Threados t=new Threados();
+		Thread t1=new Thread(t);
+		
+		// t1.run(sc.nextInt()); //so its not possible to pass the argument inside the run statement
+		t1.run();
+	}
 }
