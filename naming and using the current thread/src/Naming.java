cf405fd0cafa2d36ef9b31af9ac7d1ead4104099
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-21 15:13:07 +0530

    naming a thread

--- naming and using the current thread/src/Naming.java
+++ naming and using the current thread/src/Naming.java
@@ -4,7 +4,29 @@ public class Naming extends Thread{
 		super(name);
 	}
 	public void run() {
-		System.out.println("executing the thread");
+		try {
+			Thread.sleep(1000);
+			System.out.println("executing the thread");
+		}
+		catch(InterruptedException e) {
+			System.out.println(e);
+			
+		}
+		
 	}
 
+}
+class Naming1{
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string: ");
+		Naming n=new Naming(sc.next());
+		System.out.println("enter the string: ");
+		Naming n1=new Naming(sc.next());
+		System.out.println("thread n:- "+n.getName());
+		System.out.println("thread n1:- "+n1.getName());
+		n.start();
+		n1.start();
+		sc.close();
+	}
 }
