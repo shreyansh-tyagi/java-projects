06ed77676143617acaf8f7cdac5cd6d82d2c3584
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-22 11:25:31 +0530

    destroy thread

--- destroy thread/src/DestroyThread.java
+++ destroy thread/src/DestroyThread.java
@@ -1,4 +1,21 @@
-
-public class DestroyThread {
+import java.util.*;
+public class DestroyThread extends Thread{
+	DestroyThread(String name,ThreadGroup tg){
+		super(tg,name);
+		start();
+	}
+	public void run() {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			try {
+				Thread.sleep(500);
+			}
+			catch(InterruptedException e) {
+				System.out.println(e);
+			}
+		}
+		System.out.println(Thread.currentThread().getName()+"thread has finished executing");
+	}
 
 }
