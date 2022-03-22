4b795b83d7548ff639cc565af4fa6d0c5bd871f6
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-22 15:56:23 +0530

    java collection program

--- java collection programs/arraylist/src/arraylist/Arraylistjava.java
+++ java collection programs/arraylist/src/arraylist/Arraylistjava.java
@@ -3,15 +3,16 @@ import java.util.*;
 public class Arraylistjava {
 	public static void main(String args[]) {
 		Scanner sc =new Scanner(System.in);
+		System.out.println("enter the size of array list: ");
 		int n=sc.nextInt();
 		ArrayList<String> l=new ArrayList<String>(); //created a array list of type string
 		for(int i=0;i<n;i++) {
 			System.out.println("enter the element into the arraylist: ");
 			l.add(sc.next());
 		}
-		Iterator it=l.iterator();
+		Iterator<String> it=l.iterator();
 		while(it.hasNext()) {
-			System.out.println(it.next());
+			System.out.println("\n"+it.next());
 		}
 		sc.close();
 		}
