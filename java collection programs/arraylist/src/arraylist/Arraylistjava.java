0be9a55dcfe0c0567ae13816c2cd5fe657456168
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-22 15:41:29 +0530

    java collection program

--- java collection programs/arraylist/src/arraylist/Arraylistjava.java
+++ java collection programs/arraylist/src/arraylist/Arraylistjava.java
@@ -1,5 +1,14 @@
 package arraylist;
-
+import java.util.*;
 public class Arraylistjava {
+	public static void main(String args[]) {
+		Scanner sc =new Scanner(System.in);
+		int n=sc.nextInt();
+		ArrayList<String> l=new ArrayList<String>(); //created a array list of type string
+		for(int i=0;i<n;i++) {
+			System.out.println("enter the element into the arraylist: ");
+			l.add(sc.next());
+		}
+		}
 
 }
