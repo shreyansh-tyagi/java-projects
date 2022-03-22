296e5bdbb3b9770c2a9f2a8b06c08794dc2769bf
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-22 12:53:30 +0530

    destroy thread

--- destroy thread/src/DestroyThread.java
+++ destroy thread/src/DestroyThread.java
@@ -28,11 +28,11 @@ class DestroyThread1{
 		ThreadGroup g=new ThreadGroup("parent group thread"); //parent thread
 		ThreadGroup g1=new ThreadGroup(g,"child group thread"); //child thread 
 		ThreadGroup g2=new ThreadGroup(g1,"child of child");
-		DestroyThread d=new DestroyThread("shreyansh",g);
+		DestroyThread d=new DestroyThread("shreyansh",g); //first parent thread will execute 
 		System.out.println("exwcuting the first thread");
-		DestroyThread d1=new DestroyThread("sunny",g1);
+		DestroyThread d1=new DestroyThread("sunny",g1); //atlast child thread will execute 
 		System.out.println("exwcuting the second thread\n");
-		DestroyThread d2=new DestroyThread("rajesh",g);
+		DestroyThread d2=new DestroyThread("rajesh",g2); //first parent thread will execute 
 		System.out.println("exwcuting the third thread\n");
 		
 		Thread[] arr=new Thread[g.activeCount()]; //creating an array of thread type
@@ -40,7 +40,9 @@ class DestroyThread1{
 		for(int i=0;i<enums;i++) {
 			System.out.println("thread "+arr[i].getName()+" is found");
 		}
-		
+		System.out.println("\nthe parent group of "+g.getName()+" is"+g.getParent().getName());
+		System.out.println("\nthe parent group of "+g1.getName()+" is"+g1.getParent().getName());
+		System.out.println("\nthe parent group of "+g2.getName()+" is"+g2.getParent().getName());
 		d.join();
 		d1.join();
 		d2.join();
