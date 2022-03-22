dc56394823fdbbf0bd44fa0a38062278abd30b10
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-22 16:54:51 +0530

    java collection program

--- java collection programs/arraylist/src/arraylist/Arraylistjava.java
+++ java collection programs/arraylist/src/arraylist/Arraylistjava.java
@@ -5,14 +5,20 @@ public class Arraylistjava {
 		Scanner sc =new Scanner(System.in);
 		System.out.println("enter the size of array list: ");
 		int n=sc.nextInt();
-		ArrayList<String> l=new ArrayList<String>(); //created a array list of type string
-		for(int i=0;i<n;i++) {
+		List<String> l=new ArrayList<String>(); //created a array list of type string
+		/*for(int i=0;i<n;i++) {
 			System.out.println("enter the element into the arraylist: ");
 			l.add(sc.next());
-		}
+		}*/
+		l.add("shrey tyagi");
 		Iterator<String> it=l.iterator();
+		
 		while(it.hasNext()) {
 			System.out.println("\n"+it.next());
+		}
+		for(String e:l)//for each
+		{
+			System.out.println(e);
 		}
 		sc.close();
 		}
