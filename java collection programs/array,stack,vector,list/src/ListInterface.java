eea8e6a0bc883686c50e14c8a4a23d0747b9d78e
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-23 12:42:44 +0530

    java collection

--- java collection programs/array,stack,vector,list/src/ListInterface.java
+++ java collection programs/array,stack,vector,list/src/ListInterface.java
@@ -1,9 +1,22 @@
 import java.util.*;
 public class ListInterface {
-	public List array(String s) {
+	public List<String> array(String s) {
 		List<String> l=new ArrayList<String>();
 		l.add(s);
 		return l;
 	}
+	public static List<String> linkedlist(List<String> l,String s){
+		List<String> l1=new LinkedList<String>();
+		l1.add(s);
+		return l1;
+	}
 
+}
+class MainArray extends ListInterface{
+
+	public static void main(String args[]) {
+		ListInterface i=new ListInterface();
+		List<String> s=i.array("shrey tyagi");
+		List<String> s1=linkedlist(s,"sunny");
+	}
 }
