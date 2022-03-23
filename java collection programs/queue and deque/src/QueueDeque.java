eb9b5f716b10a2b3c17e8220994e0a5f61594f62
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-23 16:56:35 +0530

    queue and deque in java

--- java collection programs/queue and deque/src/QueueDeque.java
+++ java collection programs/queue and deque/src/QueueDeque.java
@@ -4,11 +4,37 @@ public class QueueDeque {
 	QueueDeque(String s){
 		QueueDeque.name=s;
 	}
-	public static Queue<String> queue(){
+	public Queue<String> queue(){
 		Queue<String> q=new PriorityQueue<String>();
 		q.add(name);
+		q.add("adding the element in queue");
 		return q;
 		
 	}
+	public  Deque<String> deque(){
+		Deque<String> d=new ArrayDeque<String>();
+		d.add(name);
+		d.add("adding the element in deque");
+		return d;
+	}
 
+}
+class Mainqueue extends QueueDeque{
+	Mainqueue(String s){
+		super(s);
+	}
+	public static void main(String args[]) {
+		QueueDeque q=new QueueDeque("shrey tyagi");
+		Queue<String> qu=q.queue();
+		Deque<String> de=q.deque();
+		System.out.println("this is queue");
+		for(String s:qu) {
+			System.out.print(s);
+		}
+		System.out.println("head-> "+qu.element());
+		System.out.println("head-> "+qu.peek());
+		for(String s1:de) {
+			System.out.print(s1);
+		}
+	}
 }
