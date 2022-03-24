d400b29d0c3a8e2b14fe0f0c8e6cfc6e03d86b76
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-24 16:59:45 +0530

    array list operation

--- java collection programs/arraylist operation/src/ArrayListOperation.java
+++ java collection programs/arraylist operation/src/ArrayListOperation.java
@@ -10,7 +10,14 @@ public class ArrayListOperation {
 		a.add(name);
 		a.add(s1);
 		a.add("string to array list manually");
+		System.out.println("getting the element of getset method string: "+a.get(1));
+		a.set(1, "index 1 is changed");
 		List<Integer> a2=getset1(v1);
+		a2.set(0,101);
+		System.out.println("\nupdated array list is: ");
+		for(Integer i:a2) {
+			System.out.println(i);
+		}
 		return a;
 	}
 	public List<Integer> getset1(int v1){
