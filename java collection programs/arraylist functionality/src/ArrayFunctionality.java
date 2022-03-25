01dfdec496e7fd28b6b070fe3ba2e893c58ce167
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-25 11:24:29 +0530

    array list should be completed today

--- java collection programs/arraylist functionality/src/ArrayFunctionality.java
+++ java collection programs/arraylist functionality/src/ArrayFunctionality.java
@@ -1,4 +1,14 @@
-
+import java.util.*;
 public class ArrayFunctionality {
+	String name,gender;
+	int age;
+	float salary;
+	
+	ArrayFunctionality(String n,String g,int a,float s){
+		this.name=n;
+		this.gender=g;
+		this.age=a;
+		this.salary=s;
+	}
 
 }
