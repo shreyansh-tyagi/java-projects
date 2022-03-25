be2e6e168047159cba75b7ef749e7e7e0a330102
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-25 12:17:36 +0530

    array list should be completed today

--- java collection programs/arraylist functionality/src/ArrayFunctionality.java
+++ java collection programs/arraylist functionality/src/ArrayFunctionality.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 public class ArrayFunctionality {
 	String name,gender;
 	int age;
@@ -18,7 +19,10 @@ class MainArray{
 		ArrayFunctionality arr1=new ArrayFunctionality("shreyansh tyagi","male",24,67500);
 		ArrayFunctionality ar2=new ArrayFunctionality("shivani","female",25,30000);
 		ArrayFunctionality arr3=new ArrayFunctionality("shalu","female",23,39000);
-		addtolist(arr,arr1,ar2,arr3);
+		List<ArrayFunctionality> l=addtolist(arr,arr1,ar2,arr3);
+		System.out.println("printing the array functionality");
+		l.forEach(a->{System.out.println("name-> "+a.name+"gender-> "+a.gender+"age-> "+a.age+"salary-> "+a.salary);});
+		addelement(l);
 	}
 	public static List<ArrayFunctionality> addtolist(ArrayFunctionality arr,ArrayFunctionality arr1,ArrayFunctionality ar2,ArrayFunctionality arr3){
 		List<ArrayFunctionality> l=new ArrayList<ArrayFunctionality>();
@@ -26,5 +30,16 @@ class MainArray{
 		l.add(arr1);
 		l.add(ar2);
 		l.add(arr3);
+		return l;
+	}
+	public static List<ArrayFunctionality> addelement(List<ArrayFunctionality> l){
+		List<String> l1=new ArrayList<String>();
+		l1.add("ravi");
+		l1.add("sonu");
+		System.out.println("l2 list now: "+l1);
+		l1.add(1, "shrey at index 1");
+		
+	   
+		
 	}
 }
