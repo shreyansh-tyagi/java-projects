430b415c70c906b80b7f6e02763f1a5be15ea204
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-30 13:34:37 +0530

    formatting in java

--- java collection programs/lambda expression/src/LambdaEg.java
+++ java collection programs/lambda expression/src/LambdaEg.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.stream.Stream;
 public class LambdaEg {
 	int id,age,salary;
 	String name,gender;
@@ -8,6 +9,12 @@ public class LambdaEg {
 		this.salary=salary;
 		this.name=name;
 		this.gender=gender;
+	}
+	public void setid(int id) {
+		this.id=id;
+	}
+	public int getid() {
+		return id;
 	}
 
 }
@@ -19,9 +26,18 @@ class MainLambda{
 		list.add(new LambdaEg(31,32,52000,"aryan","male"));
 		list.add(new LambdaEg(21,34,72000,"asshi","female"));
 		
-		Collections.sort(list,(e1,e2)->{return e1.name.compareTo(e2.name);});
+		Collections.sort(list,(e1,e2)->{return e1.name.compareTo(e2.name);}); //we can use compare to method with string only 
 		System.out.println("sorting with name: ");
 		list.forEach(a->System.out.println("id: "+a.id+" age: "+a.age+" salary: "+a.salary+" name: "+a.name+" gender: "+a.gender));
+		
+		Comparator<LambdaEg> l=Comparator.comparing(LambdaEg::getid); //its not possible to use the compare to method with string
+		// we need to create the get and set method for the element with whom we want to sort 
+		Collections.sort(list,l); 
+		System.out.println("\n\nsorting with id: ");
+		list.forEach(a->System.out.println("id: "+a.id+" age: "+a.age+" salary: "+a.salary+" name: "+a.name+" gender: "+a.gender));
+		
+		Stream<LambdaEg> st=list.stream().filter(a->a.salary>60000);
+		st.forEach(a->System.out.println("name: "+a.name+" salary: "+a.salary));
 	}
 	
 }
