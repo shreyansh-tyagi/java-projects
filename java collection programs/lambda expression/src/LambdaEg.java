0f6390058487993e1171590b3ad8aa26dfeedadd
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-30 12:45:03 +0530

    formatting in java

---
+++ java collection programs/lambda expression/src/LambdaEg.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LambdaEg {
+	int id,age,salary;
+	String name,gender;
+	LambdaEg(int id,int age,int salary,String name,String gender){
+		this.id=id;
+		this.age=age;
+		this.salary=salary;
+		this.name=name;
+		this.gender=gender;
+	}
+
+}
