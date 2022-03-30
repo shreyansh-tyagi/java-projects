f6f2ac938550b0bfe08cef39cde1f547a615613d
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-30 13:04:14 +0530

    formatting in java

--- java collection programs/lambda expression/src/LambdaEg.java
+++ java collection programs/lambda expression/src/LambdaEg.java
@@ -15,7 +15,13 @@ class MainLambda{
 	public static void main(String args[]) {
 		List<LambdaEg> list=new ArrayList();
 		list.add(new LambdaEg(11,21,32000,"shrey","male"));
+		list.add(new LambdaEg(9,23,62000,"shiva","male"));
+		list.add(new LambdaEg(31,32,52000,"aryan","male"));
+		list.add(new LambdaEg(21,34,72000,"asshi","female"));
 		
+		Collections.sort(list,(e1,e2)->{return e1.name.compareTo(e2.name);});
+		System.out.println("sorting with name: ");
+		list.forEach(a->System.out.println("id: "+a.id+" age: "+a.age+" salary: "+a.salary+" name: "+a.name+" gender: "+a.gender));
 	}
 	
 }
