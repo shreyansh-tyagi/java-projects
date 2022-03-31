eb37e429dbab60df128bc446ad0e3c3a2456bba6
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-31 15:50:19 +0530

    spring first program

--- java spring framework programs/SpringFirstProgram/src/main/java/com/sts/SpringFirstProgramApplication.java
+++ java spring framework programs/SpringFirstProgram/src/main/java/com/sts/SpringFirstProgramApplication.java
@@ -2,9 +2,16 @@ package com.sts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringFirstProgramApplication {
+	@RequestMapping
+	public String name() {
+		return "<h1> hello shreyansh <h1>";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFirstProgramApplication.class, args);
