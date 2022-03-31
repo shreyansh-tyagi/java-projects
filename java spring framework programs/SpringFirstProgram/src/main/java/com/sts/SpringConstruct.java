afb5a626e1f522ec2e4f33bad8d7ca122429b03b
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-31 14:28:14 +0530

    spring first program

---
+++ java spring framework programs/SpringFirstProgram/src/main/java/com/sts/SpringConstruct.java
@@ -0,0 +1,5 @@
+package com.sts;
+
+public class SpringConstruct {
+
+}
---
+++ java spring framework programs/SpringFirstProgram/src/main/java/com/sts/SpringFirstProgramApplication.java
@@ -0,0 +1,13 @@
+package com.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstProgramApplication.class, args);
+	}
+
+}
