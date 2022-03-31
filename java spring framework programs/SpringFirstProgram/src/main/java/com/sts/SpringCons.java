22898da4f4f8883776816de6f2dedff318e3561e
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-03-31 14:36:39 +0530

    spring first program

---
+++ java spring framework programs/SpringFirstProgram/src/main/java/com/sts/SpringCons.java
@@ -0,0 +1,15 @@
+package com.sts;
+
+public class SpringCons {
+	String name;
+	public void setname(String name) {
+		this.name=name;
+	}
+	public String getname() {
+		return name;
+	}
+	public void display() {
+		System.out.println("hello "+name);
+	}
+
+}
--- java spring framework programs/SpringFirstProgram/src/main/java/com/sts/SpringConstruct.java
+++
@@ -1,5 +0,0 @@
-package com.sts;
-
-public class SpringConstruct {
-
-}
