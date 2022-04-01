152d32acbfa692e6925705e6eec7532a6a78d177
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-01 12:57:09 +0530

    spring anotation

--- java spring framework programs/SpringAnotation/src/main/java/in/springboot/controller/CustomerController.java
+++ java spring framework programs/SpringAnotation/src/main/java/in/springboot/controller/CustomerController.java
@@ -3,19 +3,26 @@
  */
 package in.springboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.springboot.service.ControllerService;
-
+import in.springboot.entity.Customer;
 /**
  * @author Admin
  *
  */
-@RestController
+@RestController //used for rest api in order get put delete
 public class CustomerController {
 	
-	@Autowired
+	@Autowired // used to autowire bean on the setter method
+	
 	ControllerService service;
-
+	
+	public List<Customer> getList(){
+		return service.getlist();
+	}
+ 
 }
