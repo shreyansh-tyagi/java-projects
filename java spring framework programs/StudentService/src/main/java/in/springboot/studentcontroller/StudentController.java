d7de56723e9541d06c9d616052801b91fe86368e
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-04 00:57:04 +0530

    spring student project

--- java spring framework programs/StudentService/src/main/java/in/springboot/studentcontroller/StudentController.java
+++ java spring framework programs/StudentService/src/main/java/in/springboot/studentcontroller/StudentController.java
@@ -1,8 +1,24 @@
 package in.springboot.studentcontroller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import in.springboot.student.Student;
+import in.springboot.studentservice.StudentService;
+
+@RequestMapping
 public class StudentController {
+	
+	@Autowired
+	StudentService stservice;
+	
+	@GetMapping("/studentest")
+	public List<Student> getlist(){
+		return stservice.getstudentlist();
+	}
 	
 
 
