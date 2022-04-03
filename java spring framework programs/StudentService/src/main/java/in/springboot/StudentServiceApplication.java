96edd0389ce6817f0bcd68d961f0f7b9eb817e8d
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-04 01:49:15 +0530

    spring student project

--- java spring framework programs/StudentService/src/main/java/in/springboot/StudentServiceApplication.java
+++ java spring framework programs/StudentService/src/main/java/in/springboot/StudentServiceApplication.java
@@ -2,8 +2,9 @@ package in.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class StudentServiceApplication {
 
 	public static void main(String[] args) {
--- java spring framework programs/StudentService/src/main/java/in/springboot/studentcontroller/StudentController.java
+++ java spring framework programs/StudentService/src/main/java/in/springboot/studentcontroller/StudentController.java
@@ -4,19 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import in.springboot.student.Student;
 import in.springboot.studentservice.StudentService;
 
-@RequestMapping
+@RestController
 public class StudentController {
 	
 	@Autowired
 	StudentService stservice;
 	
-	@GetMapping("/studentest")
+	@GetMapping("/test")
 	public List<Student> getlist(){
+		System.out.println("inside controller method");
 		return stservice.getstudentlist();
 	}
 	
