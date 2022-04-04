29afc1cb0bf5329a9df0c283ebca4c82d03b7373
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-04 14:52:03 +0530

    java projects

--- java spring framework programs/SpringBean/src/main/java/in/springboot/userservice/UserService.java
+++ java spring framework programs/SpringBean/src/main/java/in/springboot/userservice/UserService.java
@@ -6,37 +6,41 @@ import in.springboot.user.User;
 
 import java.util.*;
 
-
-@Service 
+@Service
 public class UserService {
-	
-	public List<User> getlist(){
-		List<User> user=new ArrayList<>();
-		List<User> user1=new ArrayList<>();
-		
-		User u=new User(1,"user1");
+
+	public List<User> getlist() {
+		List<User> user = new ArrayList<>();
+
+		User u = new User(1, "user1");
 		user.add(u);
+
+		u = new User(2, "user2");
+		user.add(u);
+
+		u = new User(3, "user3");
+		user.add(u);
+
+		return user;
+	}
+
+	public List<User> getlistmethod() {
+		List<User> user1 = new ArrayList<>();
+
+		User u = new User(1, "user1");
 		u.setid(11);
 		u.setname("user11");
 		user1.add(u);
-		
-		u=new User(2,"user2");
-		user.add(u);
+
+		u = new User(2, "user2");
 		u.setid(12);
 		u.setname("user22");
 		user1.add(u);
-		
-		u=new User(3,"user3");
-		user.add(u);
+
+		u = new User(3, "user3");
 		u.setid(13);
 		u.setname("user33");
 		user1.add(u);
-		
-		getlistmethod(user1);
-		
-		return user;
-	}
-	public List<User> getlistmethod(List<User> user1){
 		return user1;
 	}
 
