04e4ae12a320dd263f146f44d14abf315798edbd
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-04 15:37:17 +0530

    java projects

--- java spring framework programs/SpringBean/src/main/java/in/springboot/SpringBeanApplication.java
+++ java spring framework programs/SpringBean/src/main/java/in/springboot/SpringBeanApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 
 import in.springboot.userservice.UserService;
 
@@ -19,8 +20,13 @@ public class SpringBeanApplication {
 		
 		
 		//one way of using and calling beans is by using the class name in which bean is defined 
-		UserService u=ap.getBean(UserService.class);
-		System.out.println("printing the user created list using constructor: "+u.getlistconstruct());
+		UserService u=ap.getBean(UserService.class); //using the object of application context we are accessing the get bean method and then passing the class name as a parameter
+		System.out.println("printing the list created using constructor: "+u.getlistconstruct());
+		
+		//by using the alias name 
+		UserService u1=(UserService) ap.getBean("bean1");
+		System.out.println("printing the list created using constructor using alisa: "+u1.getlistconstruct());
+		((AbstractApplicationContext) ap).close();
 	}
 
 }
