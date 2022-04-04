22bc302d73dde259eecaed4b97c608ccbacfbf44
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-04 15:16:27 +0530

    java projects

--- java spring framework programs/SpringBean/src/main/java/in/springboot/SpringBeanApplication.java
+++ java spring framework programs/SpringBean/src/main/java/in/springboot/SpringBeanApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import in.springboot.userservice.UserService;
+
 @SpringBootApplication
 public class SpringBeanApplication {
 
@@ -15,6 +17,10 @@ public class SpringBeanApplication {
 		//here application context is the interface of the bean factory object and annotation configuration
 		// application context is the class of application context interface 
 		
+		
+		//one way of using and calling beans is by using the class name in which bean is defined 
+		UserService u=ap.getBean(UserService.class);
+		System.out.println("printing the user created list using constructor: "+u.getlistconstruct());
 	}
 
 }
