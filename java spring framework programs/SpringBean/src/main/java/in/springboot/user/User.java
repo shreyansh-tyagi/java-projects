044ece934b4e53267db3545b1cc111dc5fd31c0d
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-04 14:28:39 +0530

    using bean  anatotion

--- java spring framework programs/SpringBean/src/main/java/in/springboot/user/User.java
+++ java spring framework programs/SpringBean/src/main/java/in/springboot/user/User.java
@@ -3,25 +3,30 @@ package in.springboot.user;
 public class User {
 	int id;
 	String name;
-	User(int id ,String name){
-		this.id=id;
-		this.name=name;
+
+	public User(int id, String name) {
+		this.id = id;
+		this.name = name;
 	}
-	 public int getid() {
-		 return id;
-	 }
-	 public void setid(int id) {
-		 this.id=id;
-	 }
-	 public String name() {
-		 return name;
-	 }
-	 public void setname(String name) {
-		 this.name=name;
-	 }
-	 public String toString() {
-		 return ("id: "+id+"name: "+name);
-	 }
-	
+
+	public int getid() {
+		return id;
+	}
+
+	public void setid(int id) {
+		this.id = id;
+	}
+
+	public String getname() {
+		return name;
+	}
+
+	public void setname(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return ("id: " + id + "name: " + name);
+	}
 
 }
--- java spring framework programs/SpringBean/src/main/java/in/springboot/userservice/UserService.java
+++ java spring framework programs/SpringBean/src/main/java/in/springboot/userservice/UserService.java
@@ -1,5 +1,36 @@
 package in.springboot.userservice;
 
+import org.springframework.stereotype.Service;
+
+import in.springboot.user.User;
+
+import java.util.*;
+
+
+@Service 
 public class UserService {
+	
+	public List<User> getlist(){
+		List<User> user=new ArrayList<>();
+		List<User> user1=new ArrayList<>();
+		
+		User u=new User(1,"user1");
+		user.add(u);
+		u.setid(11);
+		u.setname("user11");
+		user1.add(u);
+		
+		u=new User(2,"user2");
+		user.add(u);
+		u.setid(12);
+		u.setname("user22");
+		user1.add(u);
+		
+		u=new User(3,"user3");
+		user.add(u);
+		u.setid(13);
+		u.setname("user33");
+		user1.add(u);
+	}
 
 }
