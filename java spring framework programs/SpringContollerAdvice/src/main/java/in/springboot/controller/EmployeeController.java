fe7a29bec363608df3abcb437a29ea7ba1f74426
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-06 23:25:08 +0530

    controller advice

--- java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/controller/EmployeeController.java
+++ java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.springboot.entity.Employee;
@@ -20,5 +21,9 @@ public class EmployeeController {
 	}
 
 	@GetMapping("/employee/{id}")
+	public Employee get(@PathVariable Integer id) {
+		return service.getEmployee(id);
+		
+	}
 	
 }
---
+++ java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/controller/ProductController.java
@@ -0,0 +1,5 @@
+package in.springboot.controller;
+
+public class ProductController {
+
+}
