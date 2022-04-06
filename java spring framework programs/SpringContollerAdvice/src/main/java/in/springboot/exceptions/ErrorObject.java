cd9f6d088332587fad359f77bf24b2a160bd040f
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-06 19:02:48 +0530

    controller advice exception handling

---
+++ java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/exceptions/ErrorObject.java
@@ -0,0 +1,49 @@
+package in.springboot.exceptions;
+
+public class ErrorObject {
+	int status;
+	String message;
+	long timestamp;
+	/**
+	 * @return the status
+	 */
+	public int getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the timestamp
+	 */
+	public long getTimestamp() {
+		return timestamp;
+	}
+	/**
+	 * @param timestamp the timestamp to set
+	 */
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public ErrorObject (int status,String message,long timestamp) {
+		this.status=status;
+		this.message=message;
+		this.timestamp=timestamp;
+	}
+
+}
