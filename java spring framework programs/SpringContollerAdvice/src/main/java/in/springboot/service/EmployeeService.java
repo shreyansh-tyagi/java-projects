cae2d6378aae519deeb059bf1882ba8cc6774bfb
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-06 19:30:03 +0530

    controller advice exception handling

--- java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/service/EmployeeService.java
+++ java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/service/EmployeeService.java
@@ -2,13 +2,16 @@ package in.springboot.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import in.springboot.entity.Employee;
 import in.springboot.exceptions.NoDataFoundException;
+import in.springboot.exceptions.ResouceNotFoundException;
 
 public class EmployeeService {
+	List<Employee> list=new ArrayList<>();
 	public List<Employee> getlist(){
-		List<Employee> list=new ArrayList<>();
+		
 		list.add(new Employee(1, "Employee 1", 28, "India"));
 		list.add(new Employee(2, "Employee 2", 29, "India"));
 		list.add(new Employee(3, "Employee 3", 30, "India"));
@@ -18,6 +21,15 @@ public class EmployeeService {
 		if(list.size()>0)
 			return list;
 		throw new NoDataFoundException("there is no data available inside the list, list is empty");
+	}
+	
+	public Employee getEmployee(Integer id) {
+		Optional<Employee> theEmployee=list.stream().filter(e->e.getId()==id).findFirst();
+		if(!theEmployee.isPresent()) {
+			throw new ResouceNotFoundException("employee not found for the id"+id);
+		}
+		return theEmployee.get();
+		
 	}
 
 }
