c4009db073258be6c2ff06a2dbd532f90154b2d4
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-06 23:49:10 +0530

    controller advice

--- java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/controller/ProductController.java
+++ java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/controller/ProductController.java
@@ -1,5 +1,28 @@
 package in.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.springboot.entity.Product;
+import in.springboot.service.ProductService;
+
+@RestController
 public class ProductController {
+	@Autowired
+	ProductService service;
+	
+	@GetMapping("/product")
+	public List<Product> getlist(){
+		return service.getlist();
+	}
+	
+	@GetMapping("/product/{id}")
+	public Product get(@PathVariable Integer id) {
+		return service.getProduct(id);
+	}
 
 }
