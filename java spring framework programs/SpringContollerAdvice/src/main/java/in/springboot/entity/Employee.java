5ad9e1e737028eb71725b99cecb65c21773fc0ea
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-06 18:54:59 +0530

    controller advice exception handling

---
+++ java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/entity/Employee.java
@@ -0,0 +1,61 @@
+package in.springboot.entity;
+
+public class Employee {
+	int id,age;
+	String name,location;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", age=" + age + ", name=" + name + ", location=" + location + "]";
+	}
+	
+
+}
