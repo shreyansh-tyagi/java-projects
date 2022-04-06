a2f16d9636c7942cfa635e4d1b9643318bc05ad3
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-06 18:58:33 +0530

    controller advice exception handling

--- java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/entity/Product.java
+++ java spring framework programs/SpringContollerAdvice/src/main/java/in/springboot/entity/Product.java
@@ -4,6 +4,46 @@ public class Product {
 	int id;
 	String name;
 	double price;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
 	
 
 }
