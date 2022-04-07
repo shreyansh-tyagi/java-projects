a27f53d21a76fb7fce939e2c8303d39cbc665df7
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-07 16:49:55 +0530

    spring exception handler post controller

---
+++ java spring framework programs/SpringExceptionHandlerPostController/src/main/java/in/springboot/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package in.springboot.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import in.springboot.entity.Employee;
+
+public class EmployeeService {
+	List<Employee> list = new ArrayList<>();
+
+	public List<Employee> getlist() {
+
+		list.add(new Employee(1, 21, "shreyanhs"));
+		list.add(new Employee(2, 22, "shrey"));
+		list.add(new Employee(3, 23, "shreya"));
+		list.add(new Employee(4, 24, "amit"));
+
+		return list;
+	}
+
+	public Employee getEmployee(@PathVariable Integer id) {
+		Optional<Employee> theEmployee = list.stream().filter(e -> e.getId() == id).findFirst();
+		if (!theEmployee.isPresent()) {
+			throw new NoDataFoundException("employee is not present for id: " + id);
+		}
+		return theEmployee.get();
+
+	}
+
+}
