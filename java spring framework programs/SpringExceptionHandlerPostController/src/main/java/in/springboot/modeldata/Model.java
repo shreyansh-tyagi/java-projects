02d3cc76824f8aaa270ead97b7195348df988ba6
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-12 11:27:00 +0530

    request mapping

---
+++ java spring framework programs/SpringExceptionHandlerPostController/src/main/java/in/springboot/modeldata/Model.java
@@ -0,0 +1,63 @@
+package in.springboot.modeldata;
+
+public class Model {
+
+	int id,age;
+	private String name,location;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	@Override
+	public String toString() {
+		return "Model [id=" + id + ", age=" + age + ", name=" + name + ", location=" + location + "]";
+	}
+	
+	
+	
+}
