22dd5aeef0b473eb14d898ceb7ed580485da3481
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-12 15:19:08 +0530

    request mapping

--- java spring framework programs/SpringModelAttribute/src/main/java/in/springboot/entity/Customer.java
+++ java spring framework programs/SpringModelAttribute/src/main/java/in/springboot/entity/Customer.java
@@ -1,5 +1,54 @@
 package in.springboot.entity;
 
 public class Customer {
+	private String name,location;
+	int age;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", location=" + location + ", age=" + age + "]";
+	}
+	public Customer(String name, String location, int age) {
+		super();
+		this.name = name;
+		this.location = location;
+		this.age = age;
+	}
+	
 
 }
