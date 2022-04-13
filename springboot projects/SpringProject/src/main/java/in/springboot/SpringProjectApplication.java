95f5e748bd20d37a75f4d87f890baef17f9570e5
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-13 12:58:10 +0530

    student java class is ready

---
+++ springboot projects/SpringProject/src/main/java/in/springboot/SpringProjectApplication.java
@@ -0,0 +1,13 @@
+package in.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+}
---
+++ springboot projects/SpringProject/src/main/java/in/springboot/student/Student.java
@@ -0,0 +1,72 @@
+package in.springboot.student;
+
+import java.time.LocalDate;
+
+public class Student {
+	private long id;
+	private String name, email;
+	private LocalDate dob;
+	private int age;
+
+	public Student() {
+		super();
+	}
+
+	public Student(long id, String name, String email, LocalDate dob, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+		this.age = age;
+	}
+
+	public Student(String name, String email, LocalDate dob, int age) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.dob = dob;
+		this.age = age;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
