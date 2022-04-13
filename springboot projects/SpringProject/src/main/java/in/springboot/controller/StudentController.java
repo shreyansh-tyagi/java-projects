7e9e70a460ce90c03902fc98aa7970f68fc21562
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-13 13:25:38 +0530

    student java class is ready

---
+++ springboot projects/SpringProject/src/main/java/in/springboot/controller/StudentController.java
@@ -0,0 +1,23 @@
+package in.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.springboot.service.StudentService;
+import in.springboot.student.Student;
+
+@RestController
+public class StudentController {
+	
+	@Autowired
+	StudentService service;
+	
+	@GetMapping("/student")
+	public List<Student> getlist(){
+		return service.getlist();
+	} 
+
+}
