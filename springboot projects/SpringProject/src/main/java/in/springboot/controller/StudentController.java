d936bf8c832f5ee755bfe7e24b9e366e2c7b11ff
Shreyansh Tyagi <71514413+shreyansh-tyagi@users.noreply.github.com>
2022-04-25 11:58:45 +0530

    java path variable and delete the content from the database

--- springboot projects/SpringProject/src/main/java/in/springboot/controller/StudentController.java
+++ springboot projects/SpringProject/src/main/java/in/springboot/controller/StudentController.java
@@ -3,7 +3,9 @@ package in.springboot.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,10 @@ public class StudentController {
 	public String registerNewStudent(@RequestBody Student student) { 
 		service.addNewStudent(student);
 		return "Success";
+	}
+	
+	@DeleteMapping(path="{studentId}")
+	public void deleteStudent(@PathVariable("studentId") Long id) {
+		
 	}
 }
